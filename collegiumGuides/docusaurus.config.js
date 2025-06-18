9a8334d65422679bea76ac4c42761fa205f1a083
Findlad <findlad@gmail.com>
2025-06-18 09:36:15 -0600

    gettong the repo name right

--- collegiumGuides/docusaurus.config.js
+++ collegiumGuides/docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "collegium-software", // Usually your GitHub org/user name.
+  organizationName: "Collegium-software", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
   onBrokenLinks: "throw",
